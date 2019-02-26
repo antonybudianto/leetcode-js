1bc508740d011a6416d34ce3de139e0acba6f074
Antony Budianto <antonybudianto@gmail.com>
2019-02-26 15:17:20 +0700

    Create remove-duplicates-from-sorted-array-ii.js

---
+++ remove-duplicates-from-sorted-array-ii.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+  let count = 1
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] === nums[i + 1]) {
+      if (count < 2) {
+        count++
+      } else {
+        nums.splice(i, 1)
+        i--
+      }
+    } else {
+      count = 1
+    }
+  }
+}
