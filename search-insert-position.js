433fcec8a5c67add94c54f354a8dccb73749b697
Antony Budianto <antonybudianto@gmail.com>
2024-06-16 15:15:28 +0700

    Create search-insert-position.js

---
+++ search-insert-position.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  let c = 0;
+  const N = nums.length;
+  for (let i = 0; i < N; i++) {
+    console.log(i, target, nums[i]);
+    if (target === nums[i]) {
+      return i;
+    } else if (nums[i] > target) {
+      c = i;
+      break;
+    }
+  }
+  if (c === 0) {
+    if (target > nums[N - 1]) return N;
+    else {
+      return 0;
+    }
+  }
+  return c;
+};
