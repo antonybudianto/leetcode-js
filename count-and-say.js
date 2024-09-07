2b0a9ebd7eef45fe8744add92893a52072611d34
Antony Budianto <antonybudianto@gmail.com>
2024-09-07 19:02:51 +0700

    Create count-and-say.js

---
+++ count-and-say.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var countAndSay = function(n) {
+    function rle(c) {
+        const mapTmp = {}
+        const s = c + ''
+        const N = s.length
+
+        if (c===1) {
+            return '1'
+        }
+       
+        let append=''
+        let lastChar=s[0]
+        let lastCt = 0
+        for(let i=0;i<N;i++) {
+            if (lastChar === s[i]) {
+                lastCt++
+            } else {
+                append += lastCt+lastChar
+                lastCt = 1
+                lastChar = s[i]
+            }
+            
+            if (i===N-1) {
+                append += lastCt+lastChar
+            }
+        }
+        return append
+    }
+
+    let tmp=''
+    if (n===1) {
+        return '1'
+    }
+    for(let i=0;i<n;i++) {
+        tmp = rle(tmp ? tmp : 1)
+    }
+    
+    return tmp
+};
